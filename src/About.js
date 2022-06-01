4aa0cb023819fa1b83de87e8963772be461b2f9f
Luis Sandoval <luismsandoval96@gmail.com>
2022-05-31 17:56:36 -0700

    connected front end to database, bootstrap carousel for the data

--- src/About.js
+++ src/About.js
@@ -1,10 +1,22 @@
 import { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
 
 class Profile extends Component {
 
   render() {
     /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    return (
+      <Container>
+        <Card bg={'light'} border={'primary'} className='text-center mb-2' style={{ width: '18rem'}}>
+          <Card.Img src='img/headshot.jpg'/>
+          <Card.Body>
+            <Card.Header>Luis Sandoval</Card.Header>
+            <Card.Text>I am software dev hello there</Card.Text>
+          </Card.Body>
+        </Card>
+      </Container>
+    )
   }
 };
 
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import Carousel from 'react-bootstrap/Carousel'
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -10,19 +12,32 @@ class BestBooks extends React.Component {
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
-  render() {
+  async componentDidMount() {
+    let url = `http://localhost:3001/books`;
+    const response = await axios.get(url);
+    this.setState({ books: response.data })
+  }
 
+  render() {
+    console.log(this.state.books);
     /* TODO: render all the books in a Carousel */
 
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
+        <Carousel>
+          {this.state.books.length ? this.state.books.map(value => (
+            <Carousel.Item key={value._id}>
+              <Carousel.Caption>
+                <h3>{value.title}</h3>
+                <p>{value.description}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          )) : (
+            <h3>No Books Found :(</h3>
+          )
+          }
+        </Carousel>
       </>
     )
   }
